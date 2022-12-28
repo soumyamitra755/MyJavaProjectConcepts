e95d4b63d6f004b506118bc1de929d40c79f21a8
soumya <soumyamitra755@gmail.com>
2022-12-28 12:57:33 +0530

    My First Java Learnings-- Methods and Functions Concepts

--- src/soumya/Functions/VarArgs.java
+++ src/soumya/Functions/VarArgs.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 
 public class VarArgs {
     public static void main(String[] args) {
-      fun(2,3,4,5,6,7,34,45,56);
-      multiple(1,3,"Soumya","Suchandra");
+      //fun(2,3,4,5,6,7,34,45,56);
+      multiple(1,3,"Soumya","Mitra");
     }
     static void multiple(int a, int b, String ...v){
         System.out.println(Arrays.toString(v));
-        System.out.println(Arrays.toString(new int[]{a}));
+       // System.out.println(Arrays.toString(new int[]{a}));
 
     }
     static void fun(int ... v){
